b347b7530ce4557dc0de42dc94652da36fc69100
Mahmoud Hashemi <mahmoud@readfaction.com>
2012-11-15 04:51:11 -0800

    hello, workles!

--- workles.py
+++ workles.py
@@ -1,75 +1,118 @@
 from __future__ import unicode_literals
 
+import inspect
+
 from werkzeug.wrappers import Request, Response
 from werkzeug.routing import Map, Rule
 from werkzeug.exceptions import HTTPException, NotFound
 from werkzeug.wsgi import SharedDataMiddleware
 from werkzeug.utils import redirect
 
-class PluginDict(dict):
-    pass
+def get_arg_names(func):
+    args, varargs, kw, defaults = inspect.getargspec(func)
+    for a in args:
+        if not isinstance(a, basestring):
+            raise TypeError('does not support anonymous tuple arguments')
+    return args
 
+# "plugins" -> "resources" ?
 class WorklesBase(object):
-    def __init__(self, routes=None, plugins=None, default_renderer=None):
-        self.routes = routes  # allow passing in a Map()
+    def __init__(self, routes=None, plugins=None, render_factory=None):
+        self.routes = []
+        self.endpoint_args = {}
+        for r in routes:
+            # allow passing in a Map()
+            nr = r.empty()
+            if hasattr(nr, 'bind_render'):
+                nr.bind_render(render_factory)
+            self.routes.append(nr)
+            self.endpoint_args[nr.endpoint] = get_arg_names(nr.endpoint)
 
-        self.plugins = PluginDict(plugins)
-        self.plugins.__dict__.update(self.plugins)
+        self.plugins = dict(plugins)
 
-        self.default_renderer = default_renderer
-
+        self.render_factory = render_factory
         self.url_map = self.make_url_map()
 
     def make_url_map(self):
         ret = Map()  # support for options?
-        for r in self.routes:
-            ret.add(r.empty())
+        for rule in self.routes:
+            r = rule.empty()
+            ret.add(r)
+            endpoint_args = set(self.endpoint_args[r.endpoint])
+            url_args = r.arguments
+            app_args = self.injectable_names
+            common_args = url_args & app_args
+            if common_args:
+                raise ValueError('Route args conflict with app args: ' + \
+                                 repr(common_args) + ' (' + r.rule + ')')
+            available_args = url_args | app_args
+            unresolved_args = endpoint_args - available_args
+            if unresolved_args:
+                raise ValueError('Route endpoint has unresolved args: ' + \
+                                 repr(unresolved_args)+' ('+ r.rule +')')
         return ret
 
-    def add_route(self, route):
-        #TODO
-        raise NotImplemented
+    @property
+    def injectable_names(self):
+        return set(self.plugins.keys() + ['request', 'app'])
 
-    #def add_renderer(self, key, render_func, is_default=False):
-    #    self.renderers[key] = render_func
-    #    if is_default:
-    #        self.default_renderer = render_func
+    def get_rules(self, r_map):
+        for rf in self.routes:
+            for rule in rf.get_rules(r_map):
+                rule = rule.empty()
+                yield rule
 
-    def __call__(self, environ, start_response):
-        request = Request(environ)
+    def match(self, request):
         adapter = self.url_map.bind_to_environ(request.environ)
+        route, values = adapter.match(return_rule=True)
+        request.path_params = values
+        injectables = dict(self.plugins)
+        injectables['request'] = request
+        injectables['app'] = self
+        injectables.update(values)
+        ep_arg_names = self.endpoint_args[route.endpoint]
+        ep_kwargs = dict([(k, v) for k, v in injectables.items()
+                          if k in ep_arg_names])
+        return route, ep_kwargs
+
+    def respond(self, request):
         try:
-            route, values = adapter.match(return_rule=True)
-            ep_res = route.endpoint(self, request, **values)
+            route, ep_kwargs = self.match(request)
+            ep_res = route.endpoint(**ep_kwargs)
         except (HTTPException, NotFound) as e:
             return e
 
         if isinstance(ep_res, Response):
-            return ep_res(environ, start_response)
+            return ep_res
+        elif callable(route.__dict__.get('render')):
+            return route.render(ep_res)
+        else:
+            return HTTPException('no renderer registered for ' + repr(route) + \
+                                 ' and no Response returned')
+        #TODO: default renderer?
 
-        if hasattr(route, 'renderer'):
-            if callable(route.renderer):
-                return route.renderer(ep_res)(environ, start_response)
-            else:
-                # TODO
-                try:
-                    renderer = self.renderers[ep.renderer]
-                except KeyError, TypeError:
-                    return HttpException('No renderer found for '+repr(ep.renderer))
-        elif self.default_renderer:
-            return self.default_renderer(ep_res)(environ, start_response)
-        print 'passthru', repr(ep_res)
-        return ep_res
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+        response = self.respond(request)
+        return response(environ, start_response)
+
 
 class Route(Rule):
-    def __init__(self, rule_str, endpoint, renderer, *a, **kw):
+    def __init__(self, rule_str, endpoint, render_arg=None, *a, **kw):
         super(Route, self).__init__(rule_str, *a, endpoint=endpoint, **kw)
-        self.renderer = renderer
 
-    def empty(self):
-        ret = super(Route, self).empty()
-        ret.renderer = self.renderer  # ret.__dict__.update(self.__dict__)?
+        self.render = None
+        self.render_arg = render_arg
+        if callable(render_arg):
+            self.render = render_arg
+
+    def empty(self, keep_render=True):
+        ret = Route(self.rule, self.endpoint, self.render_arg)
+        ret.__dict__.update(super(Route, self).empty().__dict__)
+        if keep_render:
+            ret.render = self.render
         return ret
 
-def default_response():
-    pass
+    def bind_render(self, render_factory):
+        if self.render_arg is not None:
+            self.render = render_factory(self.render_arg)
