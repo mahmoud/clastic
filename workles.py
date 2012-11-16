454aaaf5ef21aa5c2343beaf5bcb36d815801a16
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2012-11-16 05:27:57 -0800

    the beginnings of middleware and subapplications

--- workles.py
+++ workles.py
@@ -1,12 +1,17 @@
 from __future__ import unicode_literals
 
 import inspect
+import collections
 
 from werkzeug.wrappers import Request, Response
 from werkzeug.routing import Map, Rule
 from werkzeug.exceptions import HTTPException, NotFound
 from werkzeug.wsgi import SharedDataMiddleware
 from werkzeug.utils import redirect
+
+from werkzeug.routing import parse_rule
+
+RESERVED_ARGS = ['req', 'request', 'application']
 
 def get_arg_names(func):
     args, varargs, kw, defaults = inspect.getargspec(func)
@@ -15,62 +20,52 @@ def get_arg_names(func):
             raise TypeError('does not support anonymous tuple arguments')
     return args
 
-# "plugins" -> "resources" ?
-class WorklesBase(object):
-    def __init__(self, routes=None, plugins=None, render_factory=None):
+
+# turn into combination Map/RuleFactory
+class WorklesBase(Map):
+    def __init__(self, routes=None, resources=None, render_factory=None,
+                 middlewares=None, **map_kwargs):
+        map_kwargs.pop('rules', None)
+        super(WorklesBase, self).__init__(**map_kwargs)
+
         self.routes = []
-        self.endpoint_args = {}
-        for r in routes:
-            # allow passing in a Map()
-            nr = r.empty()
-            if hasattr(nr, 'bind_render'):
-                nr.bind_render(render_factory)
-            self.routes.append(nr)
-            self.endpoint_args[nr.endpoint] = get_arg_names(nr.endpoint)
-
-        self.plugins = dict(plugins)
-
+        self.resources = dict(resources or {})
+        self.middlewares = list(middlewares or [])
         self.render_factory = render_factory
-        self.url_map = self.make_url_map()
+        self.endpoint_args = {}
+        self._map_kwargs = map_kwargs
+        for entry in routes:
+            rule_factory = Route.cast(entry)
+            for r in rule_factory.get_rules(self):
+                self.add_route(r)
 
-    def make_url_map(self):
-        ret = Map()  # support for options?
-        for rule in self.routes:
-            r = rule.empty()
-            ret.add(r)
-            endpoint_args = set(self.endpoint_args[r.endpoint])
-            url_args = r.arguments
-            app_args = self.injectable_names
-            common_args = url_args & app_args
-            if common_args:
-                raise ValueError('Route args conflict with app args: ' + \
-                                 repr(common_args) + ' (' + r.rule + ')')
-            available_args = url_args | app_args
-            unresolved_args = endpoint_args - available_args
-            if unresolved_args:
-                raise ValueError('Route endpoint has unresolved args: ' + \
-                                 repr(unresolved_args)+' ('+ r.rule +')')
-        return ret
+    def add_route(self, route):
+        # note: currently only works with individual routes
+        nr = route.copy()  # is copy necessary here?
+        self.add(nr)
+        return nr
 
     @property
     def injectable_names(self):
-        return set(self.plugins.keys() + ['request', 'app'])
+        return set(self.resources.keys() + RESERVED_ARGS)
 
-    def get_rules(self, r_map):
+    def get_rules(self, r_map=None):
+        if r_map is None:
+            r_map = self
         for rf in self.routes:
             for rule in rf.get_rules(r_map):
-                rule = rule.empty()
-                yield rule
+                yield rule  # is yielding bound rules bad?
 
     def match(self, request):
-        adapter = self.url_map.bind_to_environ(request.environ)
+        adapter = self.bind_to_environ(request.environ)
         route, values = adapter.match(return_rule=True)
         request.path_params = values
-        injectables = dict(self.plugins)
+        injectables = dict(self.resources)
         injectables['request'] = request
-        injectables['app'] = self
+        injectables['req'] = request
+        injectables['application'] = self
         injectables.update(values)
-        ep_arg_names = self.endpoint_args[route.endpoint]
+        ep_arg_names = route.endpoint_args
         ep_kwargs = dict([(k, v) for k, v in injectables.items()
                           if k in ep_arg_names])
         return route, ep_kwargs
@@ -84,9 +79,10 @@ class WorklesBase(object):
 
         if isinstance(ep_res, Response):
             return ep_res
-        elif callable(route.__dict__.get('render')):
+        elif callable(getattr(route, 'render', None)):
             return route.render(ep_res)
         else:
+            import pdb;pdb.set_trace()
             return HTTPException('no renderer registered for ' + repr(route) + \
                                  ' and no Response returned')
         #TODO: default renderer?
@@ -97,22 +93,151 @@ class WorklesBase(object):
         return response(environ, start_response)
 
 
+class Middleware(object):
+    unique = True
+    reorderable = True
+    provides = ()
+
+    @property
+    def name(self):
+        return self.__class__.__name__
+
+    @property
+    def overridable(self):
+        # thought: list of overridable provides?
+        return tuple(provides)
+
+    def __eq__(self, other):
+        return type(self) == type(other)
+
+    def __ne__(self, other):
+        return type(self) != type(other)
+
+    request = None
+    endpoint = None
+    render = None
+
+
+class DummyMiddleware(Middleware):
+    def __init__(self):
+        pass
+
+    def request(self, next, request):
+        print self, 'handling', id(request)
+        try:
+            ret = next()
+        except:
+            print self, 'uhoh'
+            raise
+        print self, 'hooray'
+        return ret
+
+
+def merge_middlewares(old, new):
+    old = list(old)
+    merged = list(new)
+    for mw in old:
+        if mw.unique and mw in merged:
+            if mw.reorderable:
+                continue
+            else:
+                raise ValueError('multiple inclusion of unique '
+                                 'middleware '+mw.name)
+        merged.append(mw)
+    # todo: resolve provides conflicts
+    return merged
+
+
 class Route(Rule):
     def __init__(self, rule_str, endpoint, render_arg=None, *a, **kw):
         super(Route, self).__init__(rule_str, *a, endpoint=endpoint, **kw)
+        self._middlewares = []
+        self._resources = {}
+        self.endpoint_args = get_arg_names(endpoint)
 
-        self.render = None
+        self._render = None
         self.render_arg = render_arg
         if callable(render_arg):
-            self.render = render_arg
+            self._render = render_arg
 
-    def empty(self, keep_render=True):
+    @property
+    def is_bound(self):
+        return self.map is not None
+
+    @property
+    def render(self):
+        return self._render
+
+    def empty(self):
         ret = Route(self.rule, self.endpoint, self.render_arg)
         ret.__dict__.update(super(Route, self).empty().__dict__)
-        if keep_render:
-            ret.render = self.render
         return ret
 
+    def copy(self):
+        # todo: there's probably more
+        ret = self.empty()
+        ret._render = self.render
+        ret._middlewares = list(self._middlewares)
+        return ret
+
+    def get_middlewares(self, new_middlewares=None):
+        if new_middlewares:
+            return merge_middlewares(self._middlewares, new_middlewares)
+        else:
+            return self._middlewares
+
+    def bind(self, app):
+        resources = app.__dict__.get('resources', {})
+        render_factory = app.__dict__.get('render_factory')
+        middlewares = app.__dict__.get('middlewares', [])
+
+        merged_resources = self._resources.copy()
+        merged_resources.update(resources)
+        merged_mw = self.get_middlewares(middlewares)
+
+        r_copy = self.copy()
+        try:
+            r_copy._bind_args(app, merged_resources, merged_mw)
+        except:
+            raise
+
+        self._bind_args(app, merged_resources, middlewares)
+        self.bind_render(render_factory)
+        return self
+
+    def _bind_args(self, url_map, resources, middlewares):
+        super(Route, self).bind(url_map, rebind=True)
+        url_args = self.arguments
+        endpoint_args = set(self.endpoint_args)
+        mw_args = set().union(*[set(mw.provides) for mw in middlewares])
+        app_args = set(resources.keys()) | set(mw_args) | set(RESERVED_ARGS)
+        common_args = url_args & app_args
+        if common_args:
+            raise ValueError('Route args conflict with app args: ' + \
+                             repr(common_args) + ' (' + self.rule + ')')
+        available_args = url_args | app_args
+        unresolved_args = endpoint_args - available_args
+        if unresolved_args:
+            import pdb;pdb.set_trace()
+            raise ValueError('Route endpoint has unresolved args: ' + \
+                             repr(unresolved_args)+' ('+ self.rule +')')
+
     def bind_render(self, render_factory):
-        if self.render_arg is not None:
-            self.render = render_factory(self.render_arg)
+        # control over whether or not to override?
+        if callable(render_factory) and self.render_arg is not None:
+            self._render = render_factory(self.render_arg)
+
+    @classmethod
+    def cast(cls, in_arg):
+        if isinstance(in_arg, cls):
+            return in_arg
+        elif isinstance(in_arg, Rule):
+            ret = cls(in_arg.rule, in_arg.endpoint)
+            ret.__dict__.update(in_arg.empty().__dict__)
+            return ret
+        elif isinstance(in_arg, collections.Sequence):
+            try:
+                return cls(*in_arg)
+            except TypeError:
+                pass
+        raise TypeError('incompatible Route type: ' + repr(in_arg))
