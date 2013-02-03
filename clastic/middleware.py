51490e680f474af7711a4abf964e302ba1d39133
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-02-03 02:08:03 -0800

    add ContextProcessor and SimpleContextProcessor

--- clastic/middleware.py
+++ clastic/middleware.py
@@ -4,7 +4,7 @@ from collections import defaultdict, Mapping, Iterable
 from werkzeug.utils import cached_property
 from werkzeug.wrappers import BaseResponse  # TODO: remove dependency
 
-from sinter import make_chain, get_arg_names, _VERBOSE
+from sinter import ArgSpec, make_chain, get_arg_names, _VERBOSE
 
 
 class Middleware(object):
@@ -199,6 +199,11 @@ def _create_request_inner(endpoint, render, all_args,
     return d['process_request']
 
 
+####################################
+# Actual concrete middleware follows
+####################################
+
+
 class GetParamMiddleware(Middleware):
     def __init__(self, params=None):
         # TODO: defaults?
@@ -219,3 +224,55 @@ class GetParamMiddleware(Middleware):
         for p_name, p_type in self.params.items():
             kwargs[p_name] = request.args.get(p_name, None, p_type)
         return next(**kwargs)
+
+
+class ContextProcessor(Middleware):
+    def __init__(self, required=None, defaults=None, overwrite=False):
+        required = list(required or [])
+        defaults = defaults or {}
+        self._check_params(required, defaults, overwrite)
+
+        self.required = required
+        self.defaults = defaults
+        self.overwrite = overwrite
+        self.render = self._create_render()
+
+    def _check_params(self, required, defaults, overwrite):
+        if not all([isinstance(arg, basestring) for arg in required]):
+            raise TypeError('required argument names must be strings')
+        if not all([isinstance(arg, basestring) for arg in defaults.keys()]):
+            raise TypeError('default argument names must be strings')
+        for reserved_arg in ('self', 'next', 'context'):
+            if reserved_arg in defaults or reserved_arg in required:
+                raise NameError('attempted to use reserved argument "%s"'
+                                ' in ContextProcessor.' % reserved_arg)
+        for arg in required:
+            if arg in defaults:
+                raise NameError('ambiguous argument %r appears in both '
+                                'required and default argument lists.' % arg)
+
+    def _create_render(self):
+        def process_render_context(next, context, **kwargs):
+            if not isinstance(context, Mapping):
+                return next()
+            desired_args = self.required + self.defaults.keys()
+            for arg in desired_args:
+                if not self.overwrite and arg in context:
+                    continue
+                context[arg] = kwargs.get(arg, self.defaults.get(arg))
+            return next()
+
+        _req_args = ['next', 'context'] + self.required + self.defaults.keys()
+        _def_args = dict(self.defaults)
+        process_render_context._argspec = ArgSpec(args=_req_args,
+                                                  varargs=None,
+                                                  keywords=None,
+                                                  defaults=_def_args)
+        return process_render_context
+
+
+class SimpleContextProcessor(ContextProcessor):
+    def __init__(self, *args, **kwargs):
+        defaults = dict([(a, None) for a in args])
+        defaults.update(kwargs)
+        super(SimpleContextProcessor, self).__init__(defaults=defaults)
