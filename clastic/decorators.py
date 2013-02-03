26b0dcda7eee24d2c848fc3860470c60c943a3cb
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-02-03 05:19:55 -0800

    add utility function for using normal decorators (i.e., ones that use *args/**kwargs) with clastic

---
+++ clastic/decorators.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+
+from functools import wraps
+
+from sinter import getargspec
+
+
+def clastic_decorator(subdecorator):
+    """
+    If a decorator needs to accept *args and/or **kwargs,
+    this function makes that possible by precomputing the
+    argspec of the to-be-wrapped function and propagating
+    it to the decorated version, where it is available to
+    clastic for computing dependencies.
+    """
+    @wraps(subdecorator)
+    def sinter_compatible_decorator(f):
+        argspec = getargspec(f)
+        if argspec.varargs or argspec.keywords:
+            raise TypeError('clastic does not support functions with *args'
+                            ' or **kwargs: %r' % f)
+        ret = subdecorator(f)
+        ret._argspec = argspec
+        return ret
+    return sinter_compatible_decorator
