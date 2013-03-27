f148d6d7c20386234b0d70d2b40bd1c182ecaf7d
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-03-26 22:51:01 -0700

    add a couple basic ashes tests

---
+++ clastic/tests/test_ashes_render.py
@@ -0,0 +1,32 @@
+from __future__ import unicode_literals
+import os
+from nose.tools import raises, eq_, ok_
+
+from werkzeug.test import Client
+from werkzeug.wrappers import BaseResponse
+
+from clastic import Application
+from clastic.render import ashes
+from clastic.render import AshesRenderFactory, default_response
+
+from common import hello_world_ctx, complex_context
+
+_TMPL_DIR = os.path.join(os.path.dirname(__file__), '_ashes_tmpls')
+
+
+def test_ashes():
+    ashes_render = AshesRenderFactory(_TMPL_DIR)
+    tmpl = 'basic_template.html'
+    app = Application([('/', hello_world_ctx, tmpl),
+                       ('/<name>/', hello_world_ctx, tmpl),
+                       ('/beta/<name>/', complex_context, tmpl)],
+                      render_factory=ashes_render)
+
+    c = Client(app, BaseResponse)
+    resp = c.get('/')
+    yield eq_, resp.status_code, 200
+    yield ok_, 'world' in resp.data
+
+    resp = c.get('/beta/Rajkumar/')
+    yield eq_, resp.status_code, 200
+    yield ok_, 'Rajkumar' in resp.data
