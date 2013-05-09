81e68ca702e4b6ef559bf034033817475a0b4bdc
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-05-09 01:56:08 -0700

    add StaticApplication to root package

--- clastic/__init__.py
+++ clastic/__init__.py
@@ -14,16 +14,15 @@
 """
 import server
 
-from core import (
-    RESERVED_ARGS,
-    Application,
-    SubApplication,
-    Route
-    )
+from core import (RESERVED_ARGS,
+                  Application,
+                  SubApplication,
+                  Route)
 
 from middleware import Middleware, DummyMiddleware, GetParamMiddleware
 from render import json_response, default_response
 from meta import MetaApplication
+from static import StaticApplication
 
 from werkzeug.wrappers import Request, Response
 from werkzeug.utils import redirect, append_slash_redirect
