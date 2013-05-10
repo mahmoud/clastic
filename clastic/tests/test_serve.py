353643c612414e4292d419f373c551029af93b90
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-05-10 01:41:03 -0700

    add tests for default serve behavior for new static application

--- clastic/tests/test_serve.py
+++ clastic/tests/test_serve.py
@@ -1,10 +1,16 @@
 from __future__ import unicode_literals
-from nose.tools import ok_
+from nose.tools import ok_, eq_
 
-from clastic import Application, default_response
+import os
+from werkzeug.test import Client
+
+from clastic import Application, default_response, Response
 from clastic.session import CookieSessionMiddleware
 
 from common import session_hello_world
+
+
+_CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def test_serve():
@@ -12,4 +18,10 @@ def test_serve():
     app = Application([('/', session_hello_world, default_response),
                        ('/<name>/', session_hello_world, default_response)],
                       middlewares=[cookie_session])
-    yield ok_, app.serve(_jk_just_testing=True)
+
+    yield ok_, app.serve(_jk_just_testing=True, static_path=_CUR_DIR)
+    cl = Client(app, Response)
+
+    yield eq_, cl.get('/').status_code, 200
+    yield eq_, cl.get('/_meta/').status_code, 200
+    yield eq_, cl.get('/static/test_serve.py').status_code, 200
