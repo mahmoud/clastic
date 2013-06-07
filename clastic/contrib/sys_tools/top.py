0227e7199e8ae45da0352c3e98d757e94bc211dd
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-06-07 03:09:23 -0700

    a big ole refactor in top.py

--- clastic/contrib/sys_tools/top.py
+++ clastic/contrib/sys_tools/top.py
@@ -1,11 +1,17 @@
 from __future__ import unicode_literals
 
-from clastic import Application
-from clastic.render import json_response, AshesRenderFactory
-
 import os
 if os.name != 'posix':
+    # TODO: test on windows
     raise ImportError('webtop only supports posix platforms')
+
+import clastic
+from clastic import Application, StaticApplication
+from clastic.render import json_response, AshesRenderFactory
+
+_CLASTIC_PATH = os.path.dirname(os.path.abspath(clastic.__file__))
+_ASSET_PATH = os.path.join(_CLASTIC_PATH, '_clastic_assets')
+
 import psutil
 from datetime import timedelta
 
@@ -33,70 +39,66 @@ def bytes2human(nbytes, ndigits=0):
                                                   symbol=symbol)
 
 
-def poll():
-    procs = []
-    procs_status = {}
+_TOP_ATTRS = ('pid', 'username', 'get_nice', 'get_memory_info',
+              'get_memory_percent', 'get_cpu_percent',
+              'get_cpu_times', 'name', 'status')
+
+
+def format_cpu_time(seconds):
+    # TIME+ column shows process CPU cumulative time and it
+    # is expressed as: "mm:ss.ms"
+    # TODO: does not appear to handle days. .total_seconds()?
+    ctime = timedelta(seconds=seconds)
+    return "%s:%s.%s" % (ctime.seconds // 60 % 60,
+                         str((ctime.seconds % 60)).zfill(2),
+                         str(ctime.microseconds)[:2])
+
+
+def get_process_dicts():
+    proc_dicts = []
     for p in psutil.process_iter():
         try:
-            p.dict = p.as_dict(['username', 'get_nice', 'get_memory_info',
-                                'get_memory_percent', 'get_cpu_percent',
-                                'get_cpu_times', 'name', 'status'])
-            try:
-                procs_status[str(p.dict['status'])] += 1
-            except KeyError:
-                procs_status[str(p.dict['status'])] = 1
+            proc_dicts.append(p.as_dict(_TOP_ATTRS))
         except psutil.NoSuchProcess:
             pass
-        else:
-            procs.append(p)
 
     # return processes sorted by CPU percent usage
-    processes = sorted(procs,
-                       key=lambda p: p.dict['cpu_percent'], reverse=True)
-    return (processes, procs_status)
+    proc_dicts.sort(key=lambda x: x['cpu_percent'], reverse=True)
+    return proc_dicts
 
 
 def top():
-    return {'lines': top_structured(*poll())}
+    entries = [format_dict(pd) for pd in get_process_dicts()]
+    return {'entries': entries}
 
 
-def top_structured(procs, procs_status):
-    top = []
-    for p in procs:
-        # TIME+ column shows process CPU cumulative time and it
-        # is expressed as: "mm:ss.ms"
-        if p.dict['cpu_times'] is not None:
-            ctime = timedelta(seconds=sum(p.dict['cpu_times']))
-            ctime = "%s:%s.%s" % (ctime.seconds // 60 % 60,
-                                  str((ctime.seconds % 60)).zfill(2),
-                                  str(ctime.microseconds)[:2])
-        else:
-            ctime = ''
-        if p.dict['memory_percent'] is not None:
-            p.dict['memory_percent'] = round(p.dict['memory_percent'], 1)
-        else:
-            p.dict['memory_percent'] = ''
-        if p.dict['cpu_percent'] is None:
-            p.dict['cpu_percent'] = ''
-        line = {'pid': p.pid,
-                'user': p.dict['username'][:8],
-                'ni': p.dict['nice'],
-                'virt': bytes2human(getattr(p.dict['memory_info'], 'vms', 0)),
-                'res': bytes2human(getattr(p.dict['memory_info'], 'rss', 0)),
-                'cpu': p.dict['cpu_percent'],
-                'mem': p.dict['memory_percent'],
-                'time': ctime,
-                'name': p.dict['name'] or '',
-                }
-        top.append(line)
-    return top
+def format_dict(pd):
+    ret = {}
+    ret['pid'] = pd['pid']
+    ret['time'] = ''
+    if pd['cpu_times'] is not None:
+        ret['time'] = format_cpu_time(sum(pd['cpu_times']))
+    ret['user'] = pd['username'][:12]
+    ret['ni'] = pd['nice']
+
+    mem_info = pd['memory_info']
+    ret['virt'] = bytes2human(getattr(mem_info, 'vms', 0))
+    ret['res'] = bytes2human(getattr(mem_info, 'rss', 0))
+    ret['cpu'] = '' if pd['cpu_percent'] is None else pd['cpu_percent']
+    ret['mem'] = ''
+    if pd['memory_percent'] is not None:
+        ret['mem'] = round(pd['memory_percent'], 1)
+    ret['name'] = pd['name'] or ''
+    return ret
 
 
 def create_app():
+    print _ASSET_PATH
     routes = [('/', top, 'top.html'),
+              ('/clastic_assets/', StaticApplication(_ASSET_PATH)),
               ('/json/', top, json_response)]
     arf = AshesRenderFactory(os.path.dirname(__file__))
-    app = Application(routes, None, arf)
+    app = Application(routes, {}, arf)
     return app
 
 if __name__ == '__main__':
