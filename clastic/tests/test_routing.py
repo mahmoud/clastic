7e30c31848d264fbfa23c00da235e0952d631ace
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-09-06 23:48:01 -0700

    add HTTP method route tests

--- clastic/tests/test_routing.py
+++ clastic/tests/test_routing.py
@@ -1,10 +1,12 @@
 from __future__ import unicode_literals
 from nose.tools import raises, eq_, ok_
+from json import loads
 
 from werkzeug.test import Client
 from werkzeug.wrappers import BaseResponse
 
 from clastic import Application, render_basic
+from clastic import GET, POST, PUT, DELETE
 
 
 def api(api_path):
@@ -45,4 +47,23 @@ def test_create_route_order_incr():
         app.add(r)
         yield eq_, client.get('/api/a/b').data, 'api: a/b'
         yield eq_, app.wmap._rules[-1].rule, r[0]
+    return
+
+
+def test_http_methods_success():
+    "tests route order when routes are added incrementally"
+    ep = lambda _route: repr(_route.methods)
+    routes = [GET('/', ep, render_basic),
+              POST('/', ep, render_basic),
+              PUT('/', ep, render_basic),
+              DELETE('/', ep, render_basic)]
+    app = Application(routes)
+    client = Client(app, BaseResponse)
+    methods = ('get', 'post', 'put', 'delete')
+    for method in methods:
+        req_func = getattr(client, method)
+        resp_data = req_func('/').data
+        # lololol yay eval()
+        route_methods = eval(resp_data) - set(['HEAD'])
+        yield eq_, set([method.upper()]), route_methods
     return
