cc86409f6bb5c7b37af652f4a0275b3c67fefb91
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-09-14 15:06:20 -0700

    erosion coming along

--- examples/erosion/app.py
+++ examples/erosion/app.py
@@ -2,6 +2,8 @@
 
 import os
 import sys
+import json
+from collections import OrderedDict
 
 _CUR_PATH = os.path.dirname(os.path.abspath(__file__))
 _CLASTIC_PATH = os.path.dirname(os.path.dirname(_CUR_PATH))
@@ -24,7 +26,15 @@ def add_entry(target, name=None, expiry=None, clicks=None):
     pass
 
 
-def fetch_entry(link_alias):
+def fetch_entry(link_map, link_alias):
+    try:
+        target = link_map.get_entry(link_alias)
+    except KeyError:
+        raise  # 404? 403? 402?
+    if target.startswith('/'):
+        pass  # delegate to static application
+    else:
+        pass  # use temporary redirect
     pass
 
 
@@ -34,15 +44,46 @@ def get_link_list(link_list_path=None):
     pass
 
 
-def create_app(local_root=None):
+def create_app(link_list_path=None, local_root=None):
+    link_list_path = link_list_path or os.path.join(_CUR_PATH, 'links.txt')
+    link_map = LinkMap(link_list_path)
+    resources = {'link_map': link_map}
     routes = [('/', home, 'home.html'),
               ('/submit', add_entry),
               ('/list', get_link_list, 'list.html')]
     arf = AshesRenderFactory(_CUR_PATH)
-    app = Application(routes, {}, arf)
+    app = Application(routes, resources, arf)
     return app
 
 
-if __name__ == '__main__':
+def main():
     app = create_app()
     app.serve()
+
+
+class LinkMap(object):
+    def __init__(self, path):
+        self.path = path
+        entries = _load_entries_from_file(path)
+        self.link_map = OrderedDict(entries)
+
+    def add_link(self, alias, target):
+        if alias in self.link_map:
+            raise ValueError('alias already in use %r' % alias)
+        self.link_map[alias] = target
+
+    def get_target(self, alias):
+        return self.link_map[alias]
+
+    def save(self):
+        # TODO: high-water mark appending
+        with open(self.path, 'w') as f:
+            for alias, target in self.link_map.iteritems():
+                entry = json.dumps({'alias': alias, 'target': target})
+                f.write(entry)
+                f.write('\n')
+        # sync
+
+
+if __name__ == '__main__':
+    main()
