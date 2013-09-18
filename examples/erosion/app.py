e773036d26e07e281ae978e5416f20e8e14edba8
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-09-17 22:44:00 -0700

    a buncha erosion work

--- examples/erosion/app.py
+++ examples/erosion/app.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import json
+import time
 import string
 import codecs
 from collections import OrderedDict
@@ -33,6 +34,15 @@ _CHAR_IDX_MAP = dict((c, i) for i, c in enumerate(_CHAR_LIST))
 _CHARSET_LEN = len(_CHAR_LIST)
 
 
+_NEVER = object()
+_EXPIRY_MAP = {'mins': 5 * 60,
+               'hour': 1 * 60 * 60,
+               'day': 24 * 60 * 60,
+               'month': 30 * 24 * 60 * 60,
+               'never': _NEVER}
+_DEFAULT_EXPIRY = 'hour'
+
+
 def id_decode(text):
     ret = 0
     for i, c in enumerate(text[::-1]):
@@ -54,6 +64,13 @@ def home():
 
 
 def add_entry(target, alias=None, expiry=None, clicks=None):
+    expiry = expiry or _DEFAULT_EXPIRY
+    expiry_seconds = _EXPIRY_MAP[expiry]
+    if expiry_seconds is _NEVER:
+        expiry_seconds = None
+    cur_time = time.time()
+    expiry_time = int(cur_time + expiry_seconds)
+
     if target.startswith('/'):
         # check accessibility/existence of file?
         pass
@@ -85,25 +102,35 @@ def create_app(link_list_path=None, local_root=None):
 
     scp = SimpleContextProcessor('local_root')
     pdm = PostDataMiddleware(['target', 'alias'])
+    redirect_home = make_redirect('/')
+    submit_route = POST('/submit', link_map.add_entry, redirect_home)
     routes = [('/', home, 'home.html'),
-              POST('/submit', add_entry)]
-    arf = AshesRenderFactory(_CUR_PATH)
+              submit_route]
+    arf = AshesRenderFactory(_CUR_PATH, keep_whitespace=False)
     app = Application(routes, resources, arf, [pdm, scp])
     return app
+
+
+def make_redirect(location='/', code=301):
+    def _redirect():
+        return redirect(location, code=code)
+    return _redirect
 
 
 def main():
     app = create_app(local_root='/tmp/')
     #app = create_app()
-    app.serve()
+    app.serve(use_lint=False)
 
 
 class LinkEntry(object):
-    def __init__(self, alias, target, expiry=None, max_count=None, count=0):
+    def __init__(self, link_id, target, alias,
+                 expiry_time=None, max_count=None, count=0):
+        self.link_id = link_id
         self.alias = alias
         self.target = target
         self.max_count = max_count
-        self.expire_time = expiry
+        self.expire_time = expiry_time
         self.count = count
 
     @classmethod
@@ -118,12 +145,42 @@ class LinkMap(object):
     def __init__(self, path):
         self.path = path
         entries = _load_entries_from_file(path)
-        self.link_map = OrderedDict(entries)
+        self.link_map = OrderedDict([(e.alias, e) for e in entries])
 
     def add_link(self, alias, target, expiry=None, max_count=None):
         if alias in self.link_map:
             raise ValueError('alias already in use %r' % alias)
         self.link_map[alias] = LinkEntry(alias, target, expiry, max_count)
+
+    def add_entry(self, target, alias=None, expiry=None, max_count=None):
+        if alias in self.link_map:
+            raise ValueError('alias already in use %r' % alias)
+        next_id = self._get_next_id()
+        if alias is None:
+            alias = id_encode(next_id)
+        expire_time = self._get_expiry_time(expiry)
+
+        entry = LinkEntry(next_id, alias, expire_time, max_count)
+        self.link_map[entry.alias] = entry
+        return entry
+
+    def _get_expiry_time(self, expire_interval_name):
+        expire_interval_name = expire_interval_name or _DEFAULT_EXPIRY
+        expiry_seconds = _EXPIRY_MAP[expire_interval_name]
+        if expiry_seconds is _NEVER:
+            expiry_time = None
+        else:
+            cur_time = time.time()
+            expiry_time = int(cur_time + expiry_seconds)
+        return expiry_time
+
+    def _get_next_id(self):
+        try:
+            last_alias = next(reversed(self.link_map))
+            last_id = self.link_map[last_alias].link_id
+        except:
+            last_id = 41660
+        return last_id + 1
 
     def get_target(self, alias):
         return self.link_map[alias]
