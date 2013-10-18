de3adfe8c48cbfb34f285476eba1952ddda798f2
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-10-18 02:14:43 -0700

    add a default expiration for the cookie session middleware

--- clastic/tests/test_session.py
+++ clastic/tests/test_session.py
@@ -14,6 +14,7 @@ from common import session_hello_world
 
 def test_cookie_session():
     cookie_session = CookieSessionMiddleware()
+    _ = repr(cookie_session)  # coverage, lol
     app = Application([('/', session_hello_world, render_basic),
                        ('/<name>/', session_hello_world, render_basic)],
                       middlewares=[cookie_session])
@@ -25,6 +26,25 @@ def test_cookie_session():
     yield eq_, resp.data, 'Hello, Kurt!'
     resp = ic.get('/')
     yield eq_, resp.data, 'Hello, Kurt!'
+
+    ic2 = Client(app, BaseResponse)
+    resp = ic2.get('/')
+    yield eq_, resp.data, 'Hello, world!'
+
+
+def test_session_expire():
+    cookie_session = CookieSessionMiddleware(expiry=0)
+    app = Application([('/', session_hello_world, render_basic),
+                       ('/<name>/', session_hello_world, render_basic)],
+                      middlewares=[cookie_session])
+    ic = Client(app, BaseResponse)
+    resp = ic.get('/')
+    yield eq_, resp.status_code, 200
+    yield eq_, resp.data, 'Hello, world!'
+    resp = ic.get('/Kurt/')
+    yield eq_, resp.data, 'Hello, Kurt!'
+    resp = ic.get('/')
+    yield eq_, resp.data, 'Hello, world!'
 
     ic2 = Client(app, BaseResponse)
     resp = ic2.get('/')
