ba4ee338a6fe83286365ae1a330583e91031ed4c
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-10-27 22:28:58 -0700

    added Application (inherits from BaseApplication), which mostly just adds .serve() convenience.

--- clastic/application.py
+++ clastic/application.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import os
 import sys
 from collections import Sequence
+from argparse import ArgumentParser
 
 from werkzeug.wrappers import BaseResponse
 from werkzeug.wrappers import Request, Response
 
+from .server import run_simple
 from .routing import (BaseRoute,
                       Route,
                       NullRoute,
@@ -133,6 +136,69 @@ class SubApplication(object):
                 yld._compile()
                 yield yld
 
+
+class Application(BaseApplication):
+    def serve(self,
+              address='0.0.0.0',
+              port=5000,
+              use_meta=True,
+              use_lint=True,
+              use_reloader=True,
+              use_debugger=True,
+              use_static=True,
+              static_prefix='static',
+              static_path=None, **kw):
+        parser = create_dev_server_parser()
+        args, _ = parser.parse_known_args()
+
+        address = args.address or address
+        port = args.port if args.port is not None else port
+        kw['use_reloader'] = args.use_reloader and use_reloader
+        kw['use_debugger'] = args.use_debugger and use_debugger
+        # kw['processes'] = args.processes or processes
+        use_meta = args.use_meta and use_meta
+        use_lint = args.use_lint and use_lint
+        use_static = args.use_static and use_static
+
+        wrapped_wsgi = self
+        if use_meta:
+            from meta import MetaApplication
+            self.add(('/_meta/', MetaApplication))
+        if use_static:
+            from static import StaticApplication
+            static_path = args.static_path or static_path or \
+                os.path.join(os.getcwd(), 'static')
+            static_prefix = args.static_prefix or static_prefix
+            static_prefix = '/' + unicode(static_prefix).lstrip('/')
+            static_app = StaticApplication(static_path)
+            self.add((static_prefix, static_app), index=0)
+        if use_lint:
+            from werkzeug.contrib.lint import LintMiddleware
+            wrapped_wsgi = LintMiddleware(wrapped_wsgi)
+        if kw.get('_jk_just_testing'):
+            return True
+        run_simple(address, port, wrapped_wsgi, **kw)
+
+
+def create_dev_server_parser():
+    parser = ArgumentParser()
+    parser.add_argument('--address')
+    parser.add_argument('--port', type=int)
+    parser.add_argument('--static-path')
+    parser.add_argument('--static-prefix')
+    parser.add_argument('--no-static', dest='use_static',
+                        action='store_false')
+    parser.add_argument('--no-meta', dest='use_meta',
+                        action='store_false')
+    parser.add_argument('--no-reloader', dest='use_reloader',
+                        action='store_false')
+    parser.add_argument('--no-debugger', dest='use_debugger',
+                        action='store_false')
+    parser.add_argument('--no-wsgi-lint', dest='use_lint',
+                        action='store_false')
+    parser.add_argument('--processes', type=int, help="number of"
+                        " processes, if you want a forking server")
+    return parser
 
 """
 Notes
