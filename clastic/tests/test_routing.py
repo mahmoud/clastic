177ea14448e13a3657ec3ad21564876fb6d915d4
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-11-01 20:00:46 -0700

    add tests for bad routes

--- clastic/tests/test_routing.py
+++ clastic/tests/test_routing.py
@@ -10,7 +10,12 @@ from clastic import Application, render_basic
 from clastic import GET, POST, PUT, DELETE
 
 from clastic.application import BaseApplication
-from clastic.routing import BaseRoute, InvalidEndpoint
+from clastic.routing import BaseRoute, Route
+from clastic.routing import InvalidEndpoint, InvalidURLPattern
+from clastic.routing import S_STRICT, S_REWRITE, S_REDIRECT
+
+
+modes = (S_STRICT, S_REWRITE, S_REDIRECT)
 
 
 def test_new_base_route():
@@ -99,3 +104,36 @@ New routing testing strategy notes
 * Raising endpoint (50x, 40x (breaking/nonbreaking))
 * GET/POST/PUT/DELETE/OPTIONS/HEAD, etc.
 """
+
+no_arg_routes = ['/',
+                 '/alpha',
+                 '/alpha/',
+                 '/beta',
+                 '/gamma/',
+                 '/delta/epsilon',
+                 '/zeta/eta/']
+
+arg_routes = ['/<theta>',
+              '/iota/<kappa>/<lambda>/mu/',
+              '/<nu:int>/<xi:float>/<omicron:unicode>/<pi:str>/',
+              '/<rho+>/'
+              '/<sigma*>/',
+              '/<tau?>/',
+              '/<upsilon:>/']
+
+broken_routes = ['alf',
+                 '/bet//',
+                 '/<cat->/',
+                 '/<very*doge>/']
+
+
+def test_broken_routes():
+    no_op = lambda: None
+    for cur_mode in modes:
+        for cur_patt in broken_routes:
+            try:
+                cur_rt = Route(cur_patt, no_op, slash_mode=cur_mode)
+            except InvalidURLPattern:
+                yield ok_, True
+            else:
+                yield ok_, False, cur_rt
