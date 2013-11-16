1ceeed11e1d9b2dc08a2df00d876e260aaa2716b
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-11-16 12:56:36 -0800

    add some options to SignedCookie

--- clastic/middleware/cookie.py
+++ clastic/middleware/cookie.py
@@ -19,13 +19,23 @@ class JSONCookie(SecureCookie):
 class SignedCookieMiddleware(Middleware):
     def __init__(self,
                  arg_name='cookie',
-                 cookie_name='clastic_session',
+                 cookie_name=None,
                  secret_key=None,
+                 domain=None,
+                 path='/',
+                 secure=False,
+                 http_only=False,
                  expiry=DEFAULT_EXPIRY):
         self.arg_name = arg_name
         self.provides = (arg_name,)
+        if cookie_name is None:
+            cookie_name = 'clastic_%s' % arg_name
         self.cookie_name = cookie_name
         self.secret_key = secret_key or self._get_random()
+        self.domain = domain  # used for cross-domain cookie
+        self.path = path  # limit cookie to given path
+        self.secure = secure  # only transmit on HTTPS
+        self.http_only = http_only  # disallow client-side (js) access
         self.expiry = expiry
 
     def request(self, next, request):
@@ -37,7 +47,12 @@ class SignedCookieMiddleware(Middleware):
             # this sort of reaches into the guts of contrib.securecookie
             # so as not to involve datetime.datetime.
             cookie['_expires'] = time.time() + self.expiry
-        cookie.save_cookie(response, key=self.cookie_name)
+        cookie.save_cookie(response,
+                           key=self.cookie_name,
+                           domain=self.domain,
+                           path=self.path,
+                           secure=self.secure,
+                           httponly=self.http_only)
         return response
 
     def _get_random(self):
