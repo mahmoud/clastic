3bc57932103f3cd8c7c3ae769423a72105dc499f
Mahmoud Hashemi <mahmoud@hatnote.com>
2017-09-21 18:42:19 -0700

    switch test_render over to py.test

--- clastic/tests/test_render.py
+++ clastic/tests/test_render.py
@@ -2,7 +2,7 @@
 
 from __future__ import unicode_literals
 import os
-from nose.tools import eq_, ok_
+import json
 
 from werkzeug.test import Client
 from werkzeug.wrappers import BaseResponse
@@ -15,12 +15,11 @@ from clastic.render import (JSONRender,
                             Table,
                             TabularRender)
 
-from common import (hello_world_str,
-                    hello_world_html,
-                    hello_world_ctx,
-                    complex_context)
+from clastic.tests.common import (hello_world_str,
+                                  hello_world_html,
+                                  hello_world_ctx,
+                                  complex_context)
 
-import json
 
 _CUR_DIR = os.path.dirname(__file__)
 
@@ -32,26 +31,26 @@ def test_json_render(render_json=None):
                        ('/<name>/', hello_world_ctx, render_json),
                        ('/beta/<name>/', complex_context, render_json)])
 
-    yield ok_, callable(app.routes[0]._execute)
-    yield ok_, callable(app.routes[0]._render)
+    assert callable(app.routes[0]._execute)
+    assert callable(app.routes[0]._render)
     c = Client(app, BaseResponse)
 
     resp = c.get('/')
-    yield eq_, resp.status_code, 200
+    assert resp.status_code == 200
     resp_data = json.loads(resp.data)
-    yield eq_, resp_data['name'], 'world'
+    assert resp_data['name'] == 'world'
 
     resp = c.get('/Kurt/')
-    yield eq_, resp.status_code, 200
+    assert resp.status_code == 200
     resp_data = json.loads(resp.data)
-    yield eq_, resp_data['name'], 'Kurt'
+    assert resp_data['name'] == 'Kurt'
 
     resp = c.get('/beta/Rajkumar/')
-    yield eq_, resp.status_code, 200
+    assert resp.status_code == 200
     resp_data = json.loads(resp.data)
-    yield eq_, resp_data['name'], 'Rajkumar'
-    yield ok_, resp_data['date']
-    yield ok_, len(resp_data) > 4
+    assert resp_data['name'] == 'Rajkumar'
+    assert resp_data['date']
+    assert len(resp_data) > 4
 
 
 def test_jsonp_render(render_json=None):
@@ -64,14 +63,14 @@ def test_jsonp_render(render_json=None):
     c = Client(app, BaseResponse)
 
     resp = c.get('/?callback=test_callback')
-    yield eq_, resp.status_code, 200
-    yield ok_, resp.data.startswith('test_callback')
-    yield ok_, 'world' in resp.data
+    assert resp.status_code == 200
+    assert resp.data.startswith('test_callback')
+    assert 'world' in resp.data
 
     resp = c.get('/?callback=test_callback')
-    yield eq_, resp.status_code, 200
-    yield ok_, resp.data.startswith('test_callback')
-    yield ok_, 'world' in resp.data
+    assert resp.status_code == 200
+    assert resp.data.startswith('test_callback')
+    assert 'world' in resp.data
 
 #def test_default_json_render():
 #    from clastic.render import render_json
@@ -86,34 +85,34 @@ def test_default_render():
                        ('/html/<name>/', hello_world_html, render_basic),
                        ('/beta/<name>/', complex_context, render_basic)])
 
-    yield ok_, callable(app.routes[0]._execute)
-    yield ok_, callable(app.routes[0]._render)
+    assert callable(app.routes[0]._execute)
+    assert callable(app.routes[0]._render)
     c = Client(app, BaseResponse)
 
     resp = c.get('/')  # test simple json with endpoint default
-    yield eq_, resp.status_code, 200
+    assert resp.status_code == 200
     resp_data = json.loads(resp.data)
-    yield eq_, resp_data['name'], 'world'
+    assert resp_data['name'] == 'world'
 
     resp = c.get('/Kurt/')  # test simple json with url param
-    yield eq_, resp.status_code, 200
+    assert resp.status_code == 200
     resp_data = json.loads(resp.data)
-    yield eq_, resp_data['name'], 'Kurt'
+    assert resp_data['name'] == 'Kurt'
 
     resp = c.get('/beta/Rajkumar/')  # test fancy json
-    yield eq_, resp.status_code, 200
+    assert resp.status_code == 200
     resp_data = json.loads(resp.data)
-    yield eq_, resp_data['name'], 'Rajkumar'
-    yield ok_, resp_data['date']
-    yield ok_, len(resp_data) > 4
+    assert resp_data['name'] == 'Rajkumar'
+    assert resp_data['date']
+    assert len(resp_data) > 4
 
     resp = c.get('/text/Noam/')  # test text
-    yield eq_, resp.status_code, 200
-    yield eq_, resp.data, 'Hello, Noam!'
+    assert resp.status_code == 200
+    assert resp.data == 'Hello, Noam!'
 
     resp = c.get('/html/Asia/')  # test basic html
-    yield eq_, resp.status_code, 200
-    yield ok_, 'text/html' in resp.headers['Content-Type']
+    assert resp.status_code == 200
+    assert 'text/html' in resp.headers['Content-Type']
 
 
 def test_custom_table_render():
@@ -128,5 +127,5 @@ def test_custom_table_render():
     c = Client(app, BaseResponse)
 
     resp = c.get('/?format=html')
-    yield eq_, resp.status_code, 200
+    assert resp.status_code == 200
     assert '<b>' in resp.data
