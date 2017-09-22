860a4da265e82e8493ad734d9becc5c51acf2363
Mahmoud Hashemi <mahmoud@hatnote.com>
2017-09-21 22:26:42 -0700

    convert test_render_error to pytest

--- clastic/tests/test_render_error.py
+++ clastic/tests/test_render_error.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-from nose.tools import raises, eq_, ok_
+from pytest import raises
 
 from werkzeug.test import Client
 from werkzeug.wrappers import BaseResponse
@@ -19,30 +19,31 @@ def odd_endpoint(number):
 def test_app_error_render():
 
     rt = Route('/<number:int>', odd_endpoint, render_basic)
-    yield eq_, rt._render_error, None
+    assert rt._render_error is None
 
     app = Application([rt])
-    # yield eq_, rt._render_error, render_error_basic
+    # assert rt._render_error is render_error_basic
 
     cl = Client(app, BaseResponse)
-    yield eq_, cl.get('/1').status_code, 200
+    assert cl.get('/1').status_code == 200
 
     err_resp = cl.get('/2')
-    yield eq_, err_resp.status_code, 500
-    yield ok_, 'not in my house' in err_resp.data
+    assert err_resp.status_code == 500
+    assert 'not in my house' in err_resp.data
 
     err_resp = cl.get('/non-int')
-    yield eq_, err_resp.status_code, 404
+    assert err_resp.status_code == 404
 
 
-@raises(NameError)
 def test_unresolved_error_render():
     class BadErrorHandler(ErrorHandler):
         def render_error(self, nopenope, **kwargs):
             return False
 
     rt = Route('/<number:int>', odd_endpoint, render_basic)
-    Application([rt], error_handler=BadErrorHandler())
+
+    with raises(NameError):
+        Application([rt], error_handler=BadErrorHandler())
 
 
 def test_broken_error_render():
@@ -54,8 +55,8 @@ def test_broken_error_render():
     app = Application([rt], error_handler=BrokenErrorHandler())
     cl = Client(app, BaseResponse)
     err_resp = cl.get('/2')
-    yield eq_, err_resp.status_code, 500
-    yield ok_, 'not in my house' in err_resp.data
+    assert err_resp.status_code == 500
+    assert 'not in my house' in err_resp.data
 
 
 def test_error_render_count():
@@ -76,33 +77,35 @@ def test_error_render_count():
     cl = Client(app, BaseResponse)
 
     err_resp = cl.get('/39')
-    yield eq_, err_resp.status_code, 200
+    assert err_resp.status_code == 200
     err_resp = cl.get('/2')
-    yield eq_, err_resp.status_code, 500
-    yield eq_, len(error_list), 1
+    assert err_resp.status_code == 500
+    assert len(error_list) == 1
 
     # reraising means the error will be handled by the default
     # handler, so no length change should occur
     err_resp = cl.get('/4/reraise')
-    yield eq_, err_resp.status_code, 500
-    yield eq_, len(error_list), 1
+    assert err_resp.status_code == 500
+    assert len(error_list) == 1
 
     err_resp = cl.get('/6/badgateway')
     #if err_resp.status_code != 502:
     #    import pdb;pdb.set_trace()
-    yield eq_, err_resp.status_code, 502
-    yield eq_, len(error_list), 2
+    assert err_resp.status_code == 502
+    assert len(error_list) == 2
 
 
-@raises(TypeError)
 def test_invalid_wsgi_wrapper():
     class InvalidWSGIWrapperEH(ErrorHandler):
         wsgi_wrapper = lambda app: lambda environ, nope: 'lol'
-    Application([], error_handler=InvalidWSGIWrapperEH())
+
+    with raises(TypeError):
+        Application([], error_handler=InvalidWSGIWrapperEH())
 
 
-@raises(TypeError)
 def test_uncallable_wsgi_wrapper():
     class UncallableWSGIWrapperEH(ErrorHandler):
         wsgi_wrapper = "this should be a callable but isn't"
-    Application([], error_handler=UncallableWSGIWrapperEH())
+
+    with raises(TypeError):
+        Application([], error_handler=UncallableWSGIWrapperEH())
