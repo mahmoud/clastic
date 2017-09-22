8cdcba98c94b50158664a535f6cc5c2b63f458ae
Mahmoud Hashemi <mahmoud@hatnote.com>
2017-09-21 22:58:28 -0700

    convert test_basic and test_serve from nose to pytest

--- clastic/tests/test_basic.py
+++ clastic/tests/test_basic.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-from nose.tools import raises, eq_, ok_
+from pytest import raises
 
 from werkzeug.test import Client
 from werkzeug.wrappers import BaseResponse
 
 from clastic.application import Application
 
-from common import hello_world, DummyMiddleware, RequestProvidesName
+from clastic.tests.common import hello_world, DummyMiddleware, RequestProvidesName
 
 
 def test_create_empty_application():
@@ -19,9 +19,9 @@ def test_create_empty_application():
 def test_create_hw_application():
     route = ('/', hello_world)
     app = Application([route])
-    yield ok_, app.routes
-    yield ok_, callable(app.routes[0]._execute)
-    yield ok_, app.routes[0]._bound_apps[0] is app
+    assert app.routes
+    assert callable(app.routes[0]._execute)
+    assert app.routes[0]._bound_apps[0] is app
 
 
 def test_single_mw_basic():
@@ -29,11 +29,11 @@ def test_single_mw_basic():
     app = Application([('/', hello_world)],
                       resources={},
                       middlewares=[dumdum])
-    yield ok_, dumdum in app.middlewares
-    yield ok_, dumdum in app.routes[0]._middlewares
+    assert dumdum in app.middlewares
+    assert dumdum in app.routes[0]._middlewares
 
     resp = Client(app, BaseResponse).get('/')
-    yield eq_, resp.status_code, 200
+    assert resp.status_code == 200
 
 
 def test_duplicate_noarg_mw():
@@ -41,26 +41,27 @@ def test_duplicate_noarg_mw():
         mw = [DummyMiddleware() for i in range(mw_count)]
         app = Application([('/', hello_world)],
                           middlewares=mw)
-        yield ok_, app
-        yield eq_, len(app.routes[0]._middlewares), mw_count
+        assert app
+        assert len(app.routes[0]._middlewares) == mw_count
 
         resp = Client(app, BaseResponse).get('/')
-        yield eq_, resp.status_code, 200
+        assert resp.status_code == 200
     return
 
 
-@raises(NameError)
 def test_duplicate_arg_mw():
-    req_provides1 = RequestProvidesName('Rajkumar')
-    req_provides2 = RequestProvidesName('Jimmy John')
-    Application([('/', hello_world)],
-                middlewares=[req_provides1,
-                             req_provides2])
+    with raises(NameError):
+        req_provides1 = RequestProvidesName('Rajkumar')
+        req_provides2 = RequestProvidesName('Jimmy John')
+        Application([('/', hello_world)],
+                    middlewares=[req_provides1,
+                                 req_provides2])
+    return
 
 
-@raises(NameError)
 def test_resource_builtin_conflict():
-    Application(resources={'next': lambda: None})
+    with raises(NameError):
+        Application(resources={'next': lambda: None})
 
 
 def test_subapplication_basic():
@@ -77,31 +78,31 @@ def test_subapplication_basic():
                       resources={'name': 'Kurt'},
                       middlewares=[dum2])
 
-    yield eq_, len(app.routes), 3
+    assert len(app.routes) == 3
 
     merged_name_app_patts = [r.pattern for r in app.routes
                              if r.pattern.startswith('/beta')]
     name_app_patts = [r.pattern for r in name_app.routes]
 
     def check_patts(app_patts, subapp_patts):
-        assert all(a.endswith(s) for a, s in zip(app_patts, subapp_patts))
+        return all(a.endswith(s) for a, s in zip(app_patts, subapp_patts))
 
     # should be the same order as name_app
-    yield eq_, len(merged_name_app_patts), len(name_app_patts)
-    yield check_patts, merged_name_app_patts, name_app_patts
+    assert len(merged_name_app_patts) == len(name_app_patts)
+    assert check_patts(merged_name_app_patts, name_app_patts)
 
-    yield eq_, len(set([r.pattern for r in app.routes])), 3
-    yield eq_, len(app.routes[0]._middlewares), 1  # middleware merging
+    assert len(set([r.pattern for r in app.routes])) == 3
+    assert len(app.routes[0]._middlewares) == 1  # middleware merging
 
     resp = Client(no_name_app, BaseResponse).get('/')
-    yield eq_, resp.data, 'Hello, world!'
+    assert resp.data == 'Hello, world!'
     resp = Client(name_app, BaseResponse).get('/')
-    yield eq_, resp.data, 'Hello, Rajkumar!'
+    assert resp.data == 'Hello, Rajkumar!'
     resp = Client(app, BaseResponse).get('/')
-    yield eq_, resp.data, 'Hello, Kurt!'
+    assert resp.data == 'Hello, Kurt!'
     resp = Client(app, BaseResponse).get('/beta/')
-    yield eq_, resp.data, 'Hello, Kurt!'
+    assert resp.data == 'Hello, Kurt!'
     resp = Client(app, BaseResponse).get('/beta/foo')
-    yield eq_, resp.data, 'Hello, Kurt!'
+    assert resp.data == 'Hello, Kurt!'
     resp = Client(app, BaseResponse).get('/larp4lyfe/')
-    yield eq_, resp.status_code, 404
+    assert resp.status_code == 404
--- clastic/tests/test_serve.py
+++ clastic/tests/test_serve.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-from nose.tools import ok_, eq_
 
 import os
+
 from werkzeug.test import Client
 
 from clastic import Application, render_basic, Response
 from clastic.middleware.cookie import SignedCookieMiddleware
-
-from common import cookie_hello_world
+from clastic.tests.common import cookie_hello_world
 
 
 _CUR_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -21,9 +20,9 @@ def test_serve():
                        ('/<name>/', cookie_hello_world, render_basic)],
                       middlewares=[cookie_mw])
 
-    yield ok_, app.serve(_jk_just_testing=True, static_path=_CUR_DIR)
+    assert app.serve(_jk_just_testing=True, static_path=_CUR_DIR)
     cl = Client(app, Response)
 
-    yield eq_, cl.get('/').status_code, 200
-    yield eq_, cl.get('/_meta/').status_code, 200
-    yield eq_, cl.get('/static/test_serve.py').status_code, 200
+    assert cl.get('/').status_code == 200
+    assert cl.get('/_meta/').status_code == 200
+    assert cl.get('/static/test_serve.py').status_code == 200
