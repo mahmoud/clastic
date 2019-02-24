c9986385d73c97d8768108bd6535ba1ca1a45f71
Mahmoud Hashemi <mahmoud@hatnote.com>
2019-02-24 00:05:48 -0800

    test cline

---
+++ clastic/tests/test_cline.py
@@ -0,0 +1,22 @@
+
+from werkzeug.test import Client
+
+from clastic import Response
+from clastic.cline import Cline
+
+def test_cline():
+    app = Cline()
+
+    @app.get('/')
+    @app.post('/')
+    @app.put('/')
+    @app.delete('/')
+    @app.patch('/')
+    @app.head('/')
+
+    def hw():
+        return 'hello, world'
+
+    cl = Client(app, Response)
+    resp = cl.get('/')
+    assert 'hello' in resp.get_data(True)
