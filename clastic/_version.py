04ec41d7dceb5cb9188ed497398dbbc451edeacb
Mahmoud Hashemi <mahmoud@hatnote.com>
2019-03-06 23:15:05 -0800

    fix version number construction

--- clastic/_version.py
+++ clastic/_version.py
@@ -2,4 +2,4 @@
 from __future__ import unicode_literals
 
 version_info = (19, 0, 0)
-__version__ = '.'.join(["%s" % part for part in version_info if part])
+__version__ = '.'.join(["%s" % part for part in version_info if part or part == 0])
