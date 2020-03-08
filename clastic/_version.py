e908877ad1837c9be1b3203dcbbca9644f87dadb
Mahmoud Hashemi <mahmoud@hatnote.com>
2020-03-07 18:39:24 -0800

    bump version for 20.0.0dev

--- clastic/_version.py
+++ clastic/_version.py
@@ -1,5 +1,5 @@
 
 from __future__ import unicode_literals
 
-version_info = (20, 0, 0, '')
+version_info = (20, 0, 1, 'dev')
 __version__ = '.'.join(["%s" % part for part in version_info if part or part == 0])
