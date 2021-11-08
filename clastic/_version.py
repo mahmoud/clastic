c034fcb4a18db0e0dd5714757e5430533449316f
Mahmoud Hashemi <mahmoud@hatnote.com>
2021-11-07 20:36:19 -0800

    bump version for 21.1.0, potentially last py2/py3 version

--- clastic/_version.py
+++ clastic/_version.py
@@ -1,5 +1,5 @@
 
 from __future__ import unicode_literals
 
-version_info = (21, 0, 1, 'dev')
+version_info = (21, 1, 0, '')
 __version__ = '.'.join(["%s" % part for part in version_info if part or part == 0])
